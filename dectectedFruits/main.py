6f1148456b87876b4183d3339c10e5a2af944762
Speedy <yann-arthur.tcheumani@epitech.eu>
2021-06-17 21:16:45 +0200

    add: base of prooject

---
+++ dectectedFruits/main.py
@@ -0,0 +1,20 @@
+import os
+import numpy
+from Marta.Network import Network
+
+
+class Mangagement:
+    def __init__(self):
+        self.all_image = []
+        self.all_response = []
+        self.network = Network()
+
+    def transform_to_array(self, image):
+        data = []
+        self.all_image.append(data)
+        pass
+
+    def open_(self):
+        pass
+
+    
